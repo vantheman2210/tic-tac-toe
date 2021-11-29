c2e6d62b44c3325ee9ffd988057f09db5d4fbc01
vantheman2210 <martinovic.ba@gmail.com>
2021-11-29 10:12:05 +0100

    updating game

--- script.js
+++ script.js
@@ -22,7 +22,10 @@ var gameBoard = (function() {
    let grid = document.getElementById("container");  
    let board = document.querySelectorAll(".board"); 
    let welcome = document.getElementById("welcome"); 
-   let start = document.getElementById("start");
+   let start = document.getElementById("start"); 
+   let endScreen = document.getElementById("screen"); 
+   let restartButton = document.getElementById("restart"); 
+   let display = document.getElementById("display");
 
    // Function for rendering gameboard to HTML and populating gameboard array
    function _render (e) { 
@@ -31,12 +34,12 @@ var gameBoard = (function() {
 
      if (targetItem.innerText === "" && activePlayer === playerX) { 
        targetItem.innerText = playerX.symbol;  
-       if(checkWinner(playerX.symbol)){console.log("winner X");}
+       if(checkWinner(playerX.symbol)){return endScreen.id = "screen-show", display.innerText = "Player X wins"}
        activePlayer = playerO;  
        gameboard.splice(targetId, 1, targetItem.innerText)        
      } else if (targetItem.innerText === "" && activePlayer === playerO) {
       targetItem.innerText = playerO.symbol;  
-      if(checkWinner(playerO.symbol)){console.log("winner O");}
+      if(checkWinner(playerO.symbol)){return endScreen.id = "screen-show", display.innerText = "Player O wins"}
       activePlayer = playerX; 
       gameboard.splice(targetId, 1, targetItem.innerText)   
      }
@@ -56,10 +59,24 @@ function checkWinner (symbol) {
      })
    })
 }
-
+// Event listener to start player vs player game
 start.addEventListener("click", () => { 
   welcome.id = "welcome-hidden";
+})  
+// Event listener for restart button
+restartButton.addEventListener("click", () => {
+  restart();
 })
+
+//function that restarts game 
+function restart () { 
+  board.forEach((element) => {element.innerText = "";}) 
+  gameboard = ["", "", "", "", "", "", "", "", ""];  
+  welcome.id = "welcome";    
+  endScreen.id = "screen"; 
+  display.innerText = ""; 
+  activePlayer = playerX;
+};
 
    return { 
      playGame
